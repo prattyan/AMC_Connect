2546e85dc0fe66066a883fc13398e522ee2e0ec1
prattyan <prattyanghosh@gmail.com>
2025-11-29 00:00:35 +0530

    back

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.post('/api/data', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
